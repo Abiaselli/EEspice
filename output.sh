a6d558b882c3629b2c3a1b504d93bb8f26e3a3f0
XuanhaoBao <s2508158@ed.ac.uk>
2025-03-13 20:35:21 +0000

    Add script to generate directory structure and file contents

---
+++ output.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define the destination folders
+FOLDERS="src"
+
+(
+  # Generate directory structure
+  echo "### FILE TREE ###"
+  # tree $FOLDERS
+  for folder in $FOLDERS; do
+    tree -L 1 "$folder"
+  done
+
+  # Generate file contents (code files only)
+  echo -e "\n### FILE CONTENTS ###"
+  find $FOLDERS -type f \( -name "*.hpp" -o -name "*.cpp" -o -name "*.h" -o -name "*.py" \) \
+    -exec sh -c 'echo -e "\n--- $1 ---\n"; cat "$1"' sh {} \;
+) > output.txt
