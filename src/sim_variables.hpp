2438841018dbc49b0d4b593a4d7f8441b8efdeb7
XuanhaoBao <s2508158@ed.ac.uk>
2025-03-14 21:27:31 +0000

    modified:   src/sim_variables.hpp

--- src/sim_variables.hpp
+++ src/sim_variables.hpp
@@ -5,33 +5,33 @@
 
 // Global variables
 /* Upper transient iteration limit for iteration count in time-step control algorithm*/
-const constexpr int ITL4 = 10;
+constexpr int ITL4 = 10;
 
 /* Scale factor for predicting*/
-const constexpr double TRTOL = 7.0;
+constexpr double TRTOL = 7.0;
 
 /* Error Charge Factor*/
-const constexpr double CHGTOL = 1.0e-14;
+constexpr double CHGTOL = 1.0e-14;
 
 /* Relative error*/
-const constexpr double RELTOL = 1.0e-3;
+constexpr double RELTOL = 1.0e-3;
 
 /* Absolute error for voltage in SPICE book*/
-const constexpr double VNTOL = 1.0e-6;
+constexpr double VNTOL = 1.0e-6;
 /* Absolute error for current in SPICE book*/
-const constexpr double ABSTOL = 1.0e-12;
+constexpr double ABSTOL = 1.0e-12;
 
 // Relative charge tolerance for truncation error
-const constexpr double relq = 0.01;
+constexpr double relq = 0.01;
 // Relative current tolerance for truncation error in SPICE OPUS
-const constexpr double lteretol = 0.01;
+constexpr double lteretol = 0.01;
 // Absolute tolerance for truncation error in SPICE OPUS
-const constexpr double lteabstol = 1.0e-6;
+constexpr double lteabstol = 1.0e-6;
 
-const constexpr double h_op = 1.0e-25; // Initial timestep in OP
+constexpr double h_op = 1.0e-25; // Initial timestep in OP
 
-const constexpr double LargeEpsilon = 1.0e-6;
-const constexpr double SmallEpsilon = 1.0e-12;
+constexpr double LargeEpsilon = 1.0e-6;
+constexpr double SmallEpsilon = 1.0e-12;
 
 BS::thread_pool pool(3);
 
@@ -42,6 +42,10 @@ bool debugMode = false;
 int NR_ITE = 0;
 int total_timepoint = 0;
 int total_NR_iteration = 0;
+
+// https://www.nist.gov/pml/weights-and-measures/si-units-temperature
+constexpr double CONSTCtoK = 273.15;
+constexpr double CONSTKtoC = -273.15;
 
 
 
