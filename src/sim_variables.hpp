90b56463b166bbc24531cf29ab799978b36a9499
XuanhaoBao <s2508158@ed.ac.uk>
2025-03-28 23:25:43 +0000

    Add physical constants to sim_variables

--- src/sim_variables.hpp
+++ src/sim_variables.hpp
@@ -43,11 +43,55 @@ int NR_ITE = 0;
 int total_timepoint = 0;
 int total_NR_iteration = 0;
 
+// Constants in eespice
+constexpr double CONSTsqrt2 = 1.4142135623730950488016887242097;
+constexpr double CONSTpi = 3.1415926535897932384626433832795;
+constexpr double CONSTnap = 2.7182818284590452353602874713527;
+constexpr double CONSTlog10e = 0.43429448190325182765112891891661;
+constexpr double CONSTlog2e = 1.4426950408889634073599246810019;
+
+/* https://physics.nist.gov/cgi-bin/cuu/Value?c
+ * value = 299 792 458 m s-1 (exact) */
+constexpr double CONSTc = 299792458;
+
 // https://www.nist.gov/pml/weights-and-measures/si-units-temperature
 constexpr double CONSTCtoK = 273.15;
 constexpr double CONSTKtoC = -273.15;
 
+/* https://physics.nist.gov/cgi-bin/cuu/Value?e
+ *                value = 1.602 176 6208 x 10-19 C
+ * standard uncertainty = 0.000 000 0098 x 10-19 C */
+constexpr double CHARGE = 1.6021766208e-19;
+
+/* https://physics.nist.gov/cgi-bin/cuu/Value?k
+ *                value = 1.380 648 52 x 10-23 J K-1
+ * standard uncertainty = 0.000 000 79 x 10-23 J K-1 */
+constexpr double CONSTboltz = 1.38064852e-23;
+
+/* https://physics.nist.gov/cgi-bin/cuu/Value?h
+ *                value = 6.626 070 040 x 10-34 J s
+ * standard uncertainty = 0.000 000 081 x 10-34 J s */
+constexpr double CONSTplanck = 6.626070040e-34;
+
 constexpr double nomTemp = 300.15; // Nominal temperature in Kelvin
+
+constexpr double CONSTmuZero = (4.0 * CONSTpi * 1E-7); /* MuZero H/m */
+
+/* epsilon zero  e0*u0*c*c=1 */
+constexpr double CONSTepsZero = (1.0 / (CONSTmuZero * CONSTc * CONSTc)); /* F/m */
+
+/* This value is not really constant over temperature and frequency, but
+ * 3.9 is the most common "all-purpose" value */
+constexpr double CONSTepsrSiO2 = 3.9;
+
+constexpr double CONSTepsSiO2 = (CONSTepsrSiO2 * CONSTepsZero);  /* epsilon SiO2 F/m */
+constexpr double REFTEMP = (27.0 + CONSTCtoK); /* 27 degrees C in K */
+
+
+constexpr double CONSTroot2 = CONSTsqrt2;
+constexpr double CONSTvt0 = CONSTboltz * REFTEMP / CHARGE;
+constexpr double CONSTKoverQ = CONSTboltz / CHARGE;
+constexpr double CONSTe = CONSTnap;
 
 
 
