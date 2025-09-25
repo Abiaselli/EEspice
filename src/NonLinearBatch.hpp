7db7ff60e07cec57f7d62c2eb9dc2fec95917e40
Xuanhao Bao <baoxuanhao2000@163.com>
2025-09-25 02:31:33 +0100

    Refactor NonLinearBatch function to streamline NMOS, PMOS, and BSIM4 processing and enhance parallel task handling

--- src/NonLinearBatch.hpp
+++ src/NonLinearBatch.hpp
@@ -32,25 +32,13 @@ std::pair<arma::mat, arma::vec> NonLinearBatch(CKTcircuit &ckt, const arma::vec 
         std::vector<std::future<std::pair<arma::mat, arma::vec>>> futures;
         futures.reserve(ckt.CKTelements.nmos.size());
 
-        for (std::size_t i = 0; i < ckt.CKTelements.nmos.size(); ++i) {
-            futures.push_back(pool_global.submit_task([&, i]() -> std::pair<arma::mat, arma::vec> {
+        for (const auto& nmos : ckt.CKTelements.nmos) {
+            futures.push_back(pool_global.submit_task([&]() -> std::pair<arma::mat, arma::vec> {
                 arma::mat local_LHS = arma::zeros<arma::mat>(LHS.n_rows, LHS.n_cols);
                 arma::vec local_RHS = arma::zeros<arma::vec>(RHS.n_elem);
 
-                auto &nmos = ckt.CKTelements.nmos[i];
-
-                if (nmos.modelType == MosfetModelType::LEVEL1){
-                    const NMOSModel nmosModel = modmap.nmosModels.at(nmos.modelName);
-                    NMOS_assigner(nmos.id, nmos.node_vd, nmos.node_vg, nmos.node_vs, nmos.node_vb, nmos.W, nmos.L, pre_NR_solution, ckt.T_nodes, local_LHS, local_RHS, nmosModel);
-                }
-                else if (nmos.modelType == MosfetModelType::BSIM4V82){
-                    const bsim4::BSIM4model &b4model = *nmos.bsim4v82Instance.BSIM4modPtr;
-                    bsim4::BSIM4V82 &b4instance = nmos.bsim4v82Instance;
-                    bsim4::BSIM4load(ckt, b4model, b4instance, ckt.spiceCompatible, pre_NR_solution, ckt.CKTtemp, ckt.CKTgmin, local_LHS, local_RHS);
-                }
-                else{
-                    throw DeviceException("Error: NMOS model type is not supported!", "UNSUPPORTED_NMOS_MODEL");
-                }
+                const NMOSModel nmosModel = modmap.nmosModels.at(nmos.modelName);
+                NMOS_assigner(nmos.id, nmos.node_vd, nmos.node_vg, nmos.node_vs, nmos.node_vb, nmos.W, nmos.L, pre_NR_solution, ckt.T_nodes, local_LHS, local_RHS, nmosModel);
 
                 return std::make_pair(local_LHS, local_RHS);
             }));
@@ -69,25 +57,13 @@ std::pair<arma::mat, arma::vec> NonLinearBatch(CKTcircuit &ckt, const arma::vec 
         std::vector<std::future<std::pair<arma::mat, arma::vec>>> futures;
         futures.reserve(ckt.CKTelements.pmos.size());
 
-        for (std::size_t i = 0; i < ckt.CKTelements.pmos.size(); ++i) {
-            futures.push_back(pool_global.submit_task([&, i]() -> std::pair<arma::mat, arma::vec> {
+        for (const auto& pmos : ckt.CKTelements.pmos) {
+            futures.push_back(pool_global.submit_task([&]() -> std::pair<arma::mat, arma::vec> {
                 arma::mat local_LHS = arma::zeros<arma::mat>(LHS.n_rows, LHS.n_cols);
                 arma::vec local_RHS = arma::zeros<arma::vec>(RHS.n_elem);
 
-                auto &pmos = ckt.CKTelements.pmos[i];
-
-                if (pmos.modelType == MosfetModelType::LEVEL1){
-                    const PMOSModel pmosModel = modmap.pmosModels.at(pmos.modelName);
-                    PMOS_assigner(pmos.id, pmos.node_vd, pmos.node_vg, pmos.node_vs, pmos.node_vb, pmos.W, pmos.L, pre_NR_solution, ckt.T_nodes, local_LHS, local_RHS, pmosModel);
-                }
-                else if (pmos.modelType == MosfetModelType::BSIM4V82){
-                    const bsim4::BSIM4model &b4model = *pmos.bsim4v82Instance.BSIM4modPtr;
-                    bsim4::BSIM4V82 &b4instance = pmos.bsim4v82Instance;
-                    bsim4::BSIM4load(ckt, b4model, b4instance, ckt.spiceCompatible, pre_NR_solution, ckt.CKTtemp, ckt.CKTgmin, local_LHS, local_RHS);
-                }
-                else{
-                    throw DeviceException("Error: PMOS model type is not supported!", "UNSUPPORTED_PMOS_MODEL");
-                }
+                const PMOSModel pmosModel = modmap.pmosModels.at(pmos.modelName);
+                PMOS_assigner(pmos.id, pmos.node_vd, pmos.node_vg, pmos.node_vs, pmos.node_vb, pmos.W, pmos.L, pre_NR_solution, ckt.T_nodes, local_LHS, local_RHS, pmosModel);
 
                 return std::make_pair(local_LHS, local_RHS);
             }));
@@ -101,6 +77,34 @@ std::pair<arma::mat, arma::vec> NonLinearBatch(CKTcircuit &ckt, const arma::vec 
         }
         merge_matrices(LHS, RHS, pmos_results);
     }
+
+    // Parallel processing of BSIM4 transistors
+    if (!ckt.CKTelements.bsim4.empty()) {
+        std::vector<std::future<std::pair<arma::mat, arma::vec>>> futures;
+        futures.reserve(ckt.CKTelements.bsim4.size());
+
+        for (const auto& bsim4 : ckt.CKTelements.bsim4) {
+            futures.push_back(pool_global.submit_task([&]() -> std::pair<arma::mat, arma::vec> {
+                arma::mat local_LHS = arma::zeros<arma::mat>(LHS.n_rows, LHS.n_cols);
+                arma::vec local_RHS = arma::zeros<arma::vec>(RHS.n_elem);
+
+                const bsim4::BSIM4model &b4model = *bsim4.bsim4v82Instance.BSIM4modPtr;
+                bsim4::BSIM4V82 &b4instance = const_cast<bsim4::BSIM4V82&>(bsim4.bsim4v82Instance);
+                bsim4::BSIM4load(ckt, b4model, b4instance, ckt.spiceCompatible, pre_NR_solution, ckt.CKTtemp, ckt.CKTgmin, local_LHS, local_RHS);
+
+                return std::make_pair(local_LHS, local_RHS);
+            }));
+        }
+
+        // Collect and merge results
+        std::vector<std::pair<arma::mat, arma::vec>> bsim4_results;
+        bsim4_results.reserve(futures.size());
+        for (auto &future : futures) {
+            bsim4_results.push_back(future.get());
+        }
+        merge_matrices(LHS, RHS, bsim4_results);
+    }
+
     for (const auto &diode : ckt.CKTelements.diodes){
         Diode_assigner(diode.nodePos, diode.nodeNeg, diode.Is, diode.VT, LHS, RHS, pre_NR_solution);
     }
