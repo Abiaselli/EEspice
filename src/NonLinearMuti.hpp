a79ca0b1e0b6a37938cf47cb9d96a3c72877190d
Xuanhao Bao <baoxuanhao2000@163.com>
2025-09-27 00:02:04 +0100

    Refactor BSIM4 parallel processing to use detach_task for improved performance and remove unnecessary matrix merging function

--- src/NonLinearMuti.hpp
+++ src/NonLinearMuti.hpp
@@ -8,15 +8,6 @@
 #include "device.hpp"
 #include "bsim4v82/bsim4v82load/bsim4v82load.hpp"
 
-// Helper function to safely merge thread-local matrices
-inline void merge_matrices(arma::mat &target_LHS, arma::vec &target_RHS,
-                          BS::multi_future<std::pair<arma::mat, arma::vec>> &thread_futures)
-{
-    for (const auto& [lhs_part, rhs_part] : thread_futures.get()) {
-        target_LHS += lhs_part;
-        target_RHS += rhs_part;
-    }
-}
 
 std::pair<arma::mat, arma::vec> NonLinearMutithreaded(CKTcircuit &ckt, const arma::vec &pre_NR_solution, 
     const std::pair<arma::mat, arma::vec> &matrixes, const Modelmap &modmap, double h)
@@ -25,7 +16,6 @@ std::pair<arma::mat, arma::vec> NonLinearMutithreaded(CKTcircuit &ckt, const arm
     arma::mat LHS = matrixes.first;
     
     arma::vec RHS = matrixes.second;
-    int matrix_size = LHS.n_rows;
 
     for (const auto &nmos : ckt.CKTelements.nmos) {
         const NMOSModel nmosModel = modmap.nmosModels.at(nmos.modelName);
@@ -36,26 +26,25 @@ std::pair<arma::mat, arma::vec> NonLinearMutithreaded(CKTcircuit &ckt, const arm
         PMOS_assigner(pmos.id, pmos.node_vd, pmos.node_vg, pmos.node_vs, pmos.node_vb, pmos.W, pmos.L, pre_NR_solution, ckt.T_nodes, LHS, RHS, pmosModel);
     }
 
-    // Parallel processing of BSIM4 transistors
+    // Parallel processing of BSIM4 transistors with direct matrix updates
     if (!ckt.CKTelements.bsim4.empty()) {
-        BS::multi_future<std::pair<arma::mat, arma::vec>> futures;
-        futures.reserve(ckt.CKTelements.bsim4.size());
+        std::mutex matrix_mutex;
 
-        for (auto& bsim4 : ckt.CKTelements.bsim4) {
-            futures.push_back(pool_global.submit_task([&bsim4, &ckt, &pre_NR_solution, matrix_size]() -> std::pair<arma::mat, arma::vec> {
-                arma::mat local_LHS = arma::zeros<arma::mat>(matrix_size, matrix_size);
-                arma::vec local_RHS = arma::zeros<arma::vec>(matrix_size);
+        // Use detach_task to avoid the overhead involved in assigning a future to the task, in order to increase performance
 
-                const bsim4::BSIM4model &b4model = *bsim4.bsim4v82Instance.BSIM4modPtr;
-                bsim4::BSIM4V82 &b4instance = bsim4.bsim4v82Instance;
-                bsim4::BSIM4load(ckt, b4model, b4instance, ckt.spiceCompatible, pre_NR_solution, ckt.CKTtemp, ckt.CKTgmin, local_LHS, local_RHS);
-
-                return std::make_pair(local_LHS, local_RHS);
-            }));
+        for (size_t i = 0; i < ckt.CKTelements.bsim4.size(); ++i) {
+            pool_global.detach_task([&, i]() -> void {
+                const bsim4::BSIM4model &b4model = *ckt.CKTelements.bsim4[i].bsim4v82Instance.BSIM4modPtr;
+                bsim4::BSIM4V82 &b4instance = ckt.CKTelements.bsim4[i].bsim4v82Instance;
+                // Calculate all the values needed for the matrices.
+                const auto stamps = bsim4::BSIM4calculateStamps(ckt, b4model, b4instance, ckt.spiceCompatible, pre_NR_solution, ckt.CKTtemp, ckt.CKTgmin);
+                // Lock and directly modify shared matrices
+                std::lock_guard<std::mutex> lock(matrix_mutex);
+                // Apply the calculated values to the LHS and RHS matrices.
+                bsim4::bsim4applyStamps(b4instance, stamps, LHS, RHS);
+            });
         }
-
-        // Merge results directly
-        merge_matrices(LHS, RHS, futures);
+        pool_global.wait();
     }
 
     for (const auto &diode : ckt.CKTelements.diodes){
