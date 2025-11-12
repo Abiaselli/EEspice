4d8d93abf9f51fd40c4fcd6c3c37eda9629c1ce8
Xuanhao Bao <baoxuanhao2000@163.com>
2025-11-12 18:00:14 +0000

    Refactor BSIM4 stamp computation to use optimized parallel processing with OpenMP

--- src/NonLinearMuti.hpp
+++ src/NonLinearMuti.hpp
@@ -4,7 +4,7 @@
 #include <future>
 #include "CKT.hpp"
 #include "device.hpp"
-#include "bsim4v82/bsim4v82load/bsim4v82load.hpp"
+#include "bsim4v82/bsim4v82load/bsim4v82loadomp.hpp"
 #include <omp.h>
 
 
@@ -26,37 +26,7 @@ std::pair<arma::mat, arma::vec> NonLinearMutithreaded(CKTcircuit &ckt, const arm
     }
 
     // Parallel BSIM4: compute stamps in parallel using OpenMP (one device per thread)
-    if (!ckt.CKTelements.bsim4.empty()) {
-        const size_t n = ckt.CKTelements.bsim4.size();
-        std::vector<bsim4::BSIM4stamp> stamps(n);
-        
-        // Phase 1: Parallel computation of stamps
-        #pragma omp parallel for
-        for (size_t i = 0; i < n; ++i) {
-            const bsim4::BSIM4model &b4model = *ckt.CKTelements.bsim4[i].bsim4v82Instance.BSIM4modPtr;
-            bsim4::BSIM4V82 &instance = ckt.CKTelements.bsim4[i].bsim4v82Instance;
-            stamps[i] = bsim4::BSIM4calculateStamps(ckt, b4model, instance, 
-                                                     ckt.spiceCompatible, pre_NR_solution, 
-                                                     ckt.CKTtemp, ckt.CKTgmin);
-        }
-        
-        // Graph coloring
-        const auto& color_groups = ckt.b4coloring.getColorGroups();
-
-        // Phase 2: Parallel application of stamps using coloring
-        #pragma omp parallel // <-- Create threads ONCE
-        {
-            for (const auto& group : color_groups) {
-                // All instances in this group can be processed in parallel
-                #pragma omp for // <-- Distribute work, threads are already active
-                for (size_t idx = 0; idx < group.size(); ++idx) {
-                    size_t i = group[idx];
-                    bsim4::BSIM4V82 &instance = ckt.CKTelements.bsim4[i].bsim4v82Instance;
-                    bsim4::bsim4applyStamps(instance, stamps[i], LHS, RHS);
-                }
-            }
-        }
-    }
+    bsim4::loadompColor4(ckt, pre_NR_solution, LHS, RHS, ckt.b4coloring);
 
 
     for (const auto &diode : ckt.CKTelements.diodes){
