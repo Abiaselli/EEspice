5ba377b37957b5ece7fad0675ff82f8a91923d60
Xuanhao Bao <baoxuanhao2000@163.com>
2025-11-20 22:21:35 +0000

    Limit file search to top-level files only in output script

--- output.sh
+++ output.sh
@@ -8,12 +8,15 @@ FOLDERS="src"
   echo "### FILE TREE ###"
   # tree $FOLDERS
   for folder in $FOLDERS; do
-    # tree -L 1 "$folder"
     tree "$folder"
+    # tree -L 1 "$folder"
   done
 
   # Generate file contents (code files only)
   echo -e "\n### FILE CONTENTS ###"
   find $FOLDERS -type f \( -name "*.hpp" -o -name "*.cpp" -o -name "*.h" -o -name "*.py" \) \
     -exec sh -c 'echo -e "\n--- $1 ---\n"; cat "$1"' sh {} \;
+  # CHANGED: Added -maxdepth 1 before -type f to limit to top-level files only
+  # find $FOLDERS -maxdepth 1 -type f \( -name "*.hpp" -o -name "*.cpp" -o -name "*.h" -o -name "*.py" \) \
+  #   -exec sh -c 'echo -e "\n--- $1 ---\n"; cat "$1"' sh {} \;
 ) > output.txt
